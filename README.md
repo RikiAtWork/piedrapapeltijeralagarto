3c5710d5b7c635d89df7e99271fe150785eb1de3
Ricardo U <ricullagu@alu.edu.gva.es>
2023-05-16 17:39:03 +0200

    V7

---
+++ README.md
@@ -0,0 +1,10 @@
+# RUA
+---
+## CFGS Desarrollo de Aplicaciones Web
+---
+### PROGRAMA PIEDRA, PAPEL, TIJERA Y LAGARTO
+
+---
+> Este programa ejecuta un codigo en el cual jugamos al tipico juego de piedra, papel, tijera y un nuevo elemento que es lagarto
+
+#### FECHA 16/05/2023 17:28
--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -1,3 +1,7 @@
+"""
+.. include:: ./README.md
+"""
+
 import random
 import sys
 
@@ -13,11 +17,25 @@ no_partidas = [[piedra, papel], [papel, tijera], [tijera, piedra],
 
 
 def generar_movimientos():
+    """
+    Metodo que genera un movimiento aleatorio y lo devuelve.
+
+    :return: String: movimiento
+    """
     movimient = random.choice(opcion)
     return movimient
 
 
 def verificar_movimientos(usario_mov, ordenador_mov):
+    """
+    Metodo que verifica que los movimientos de usuario y ordenador esten en la lista de partidas y devuelve un 1, caso
+    contrario si esta en la lista de no partidas devuelve un -1 y si no es nada de eso devuelve un 0 que sería
+    un empate.
+
+    :param usario_mov: String: movimiento usuario.
+    :param ordenador_mov: String movimiento ordenador.
+    :return: int
+    """
     if [usario_mov, ordenador_mov] in partidas:
         return 1
     elif [usario_mov, ordenador_mov] in no_partidas:
@@ -25,44 +43,47 @@ def verificar_movimientos(usario_mov, ordenador_mov):
     return 0
 
 
-print("JUEGO : Piedra, papel, tijera y lagarto")
-nombre = input("Introduce tu nombre: ")
+try:
+    print("JUEGO : Piedra, papel, tijera y lagarto")
+    nombre = input("Introduce tu nombre: ")
 
-while True:
-    opciones = input("Quieres jugar? (s/n): ")
-    if 's' in opciones.lower():
-        ordenador_movimiento = generar_movimientos()
-        while True:
-            movimiento = input("Selecciona un movimiento ('p' para piedra / 'a' para papel "
-                               "/ 't' para tijeras / 'l' para lagarto): ").lower()
-            usuario_movimiento = ""
-            if movimiento.upper() == "TERMINAR":
-                sys.exit("Tienes miedo?")
+    while True:
+        opciones = input("Quieres jugar? (s/n): ")
+        if 's' in opciones.lower():
+            ordenador_movimiento = generar_movimientos()
+            while True:
+                movimiento = input("Selecciona un movimiento ('p' para piedra / 'a' para papel "
+                                   "/ 't' para tijeras / 'l' para lagarto): ").lower()
+                usuario_movimiento = ""
+                if movimiento.upper() == "TERMINAR":
+                    sys.exit("Tienes miedo?")
 
-            if movimiento in ['p', 'a', 't', 'l']:
-                if movimiento == 'p':
-                    usuario_movimiento = piedra
-                elif movimiento == 'a':
-                    usuario_movimiento = papel
-                elif movimiento == 't':
-                    usuario_movimiento = tijera
-                elif movimiento == 'l':
-                    usuario_movimiento = lagarto
+                if movimiento in ['p', 'a', 't', 'l']:
+                    if movimiento == 'p':
+                        usuario_movimiento = piedra
+                    elif movimiento == 'a':
+                        usuario_movimiento = papel
+                    elif movimiento == 't':
+                        usuario_movimiento = tijera
+                    elif movimiento == 'l':
+                        usuario_movimiento = lagarto
 
-                print(f"Elección del ordenador: {ordenador_movimiento}")
-                print(f"Elección del usuario: {usuario_movimiento}")
+                    print(f"Elección del ordenador: {ordenador_movimiento}")
+                    print(f"Elección del usuario: {usuario_movimiento}")
 
-                if verificar_movimientos(usuario_movimiento, ordenador_movimiento) == 1:
-                    print(f"Gana el usuario, {nombre} !!!")
-                elif verificar_movimientos(usuario_movimiento, ordenador_movimiento) == -1:
-                    print("Gana el ordenador !!!")
-                elif verificar_movimientos(usuario_movimiento, ordenador_movimiento) == 0:
-                    print("Empate !!!")
-                break
-            else:
-                print("Entrada incorrecta. Vuelve a intentar.")
-    elif 'n' in opciones.lower():
-        break
-    else:
-        print('Entrada incorrecta. Vuelve a intentar.')
-    print()
+                    if verificar_movimientos(usuario_movimiento, ordenador_movimiento) == 1:
+                        print(f"Gana el usuario, {nombre} !!!")
+                    elif verificar_movimientos(usuario_movimiento, ordenador_movimiento) == -1:
+                        print("Gana el ordenador !!!")
+                    elif verificar_movimientos(usuario_movimiento, ordenador_movimiento) == 0:
+                        print("Empate !!!")
+                    break
+                else:
+                    print("Entrada incorrecta. Vuelve a intentar.")
+        elif 'n' in opciones.lower():
+            break
+        else:
+            print('Entrada incorrecta. Vuelve a intentar.')
+        print()
+except EOFError as e:
+    print(e)
